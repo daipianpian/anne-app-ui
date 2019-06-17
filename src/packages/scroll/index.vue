17e756822413a3fa041be8ec04bd85db7c86a2fb
daipianpian <daipianpian@outlook.com>
2019-06-17 13:41:26 +0800

    scroll插件优化

--- src/packages/scroll/index.vue
+++ src/packages/scroll/index.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="html">
   <div class="anne-scroll"
   :class="{'down':(state===0),'up':(state==1),refresh:(state===2),touch:touching}"
   @touchstart="touchStart($event)"
@@ -16,7 +16,7 @@
       <slot></slot>
       <footer class="load-more">
         <slot name="load-more">
-          <span>加载中……</span>
+          <span>{{onPullUpText}}</span>
         </slot>
       </footer>
     </section>
@@ -24,114 +24,122 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
-    name: "AnneScroll",
-    props: {
-      offset: {
-        type: Number,
-        default: 40
-      },
-      enableInfinite: {
-        type: Boolean,
-        default: true
-      },
-      enableRefresh: {
-        type: Boolean,
-        default: true
-      },
-      onRefresh: {
-        type: Function,
-        default: undefined,
-        required: false
-      },
-      onInfinite: {
-        type: Function,
-        default: undefined,
-        require: false
-      }
+export default {
+  name: "AnneScroll",
+  props: {
+    offset: {
+      type: Number,
+      default: 40
     },
-    data() {
-      return {
-        top: 0,
-        state: 0,
-        startY: 0,
-        touching: false,
-        infiniteLoading: false
-      }
+    enableInfinite: {
+      type: Boolean,
+      default: true
     },
-    methods: {
-      touchStart(e) {
-        this.startY = e.targetTouches[0].pageY
-        this.startScroll = this.$el.scrollTop || 0
-        this.touching = true
-      },
-      touchMove(e) {
-        if (!this.enableRefresh || this.$el.scrollTop > 0 || !this.touching) {
-          return
-        }
-        let diff = e.targetTouches[0].pageY - this.startY - this.startScroll
-        if (diff > 0) e.preventDefault()
-        this.top = Math.pow(diff, 0.8) + (this.state === 2 ? this.offset : 0)
+    enableRefresh: {
+      type: Boolean,
+      default: true
+    },
+    onRefresh: {
+      type: Function,
+      default: undefined,
+      required: false
+    },
+    onInfinite: {
+      type: Function,
+      default: undefined,
+      require: false
+    }
+  },
+  data() {
+    return {
+      top: 0,
+      state: 0,
+      startY: 0,
+      touching: false,
+      infiniteLoading: false,
+      onPullUpText: '加载中……'
+    }
+  },
+  methods: {
+    touchStart(e) {
+      this.startY = e.targetTouches[0].pageY
+      this.startScroll = this.$el.scrollTop || 0
+      this.touching = true
+    },
+    touchMove(e) {
+      if (!this.enableRefresh || this.$el.scrollTop > 0 || !this.touching) {
+        return
+      }
+      let diff = e.targetTouches[0].pageY - this.startY - this.startScroll
+      if (diff > 0) e.preventDefault()
+      this.top = Math.pow(diff, 0.8) + (this.state === 2 ? this.offset : 0)
 
-        if (this.state === 2) { // in refreshing
-          return
-        }
-        if (this.top >= this.offset) {
-          this.state = 1
-        } else {
-          this.state = 0
-        }
-      },
-      touchEnd(e) {
-        if (!this.enableRefresh) return
-        this.touching = false
-        if (this.state === 2) { // in refreshing
-          this.state = 2
-          this.top = this.offset
-          return
-        }
-        if (this.top >= this.offset) { // do refresh
-          this.refresh()
-        } else { // cancel refresh
-          this.state = 0
-          this.top = 0
-        }
-      },
-      refresh() {
+      if (this.state === 2) { // in refreshing
+        return
+      }
+      if (this.top >= this.offset) {
+        this.state = 1
+      } else {
+        this.state = 0
+      }
+    },
+    touchEnd(e) {
+      if (!this.enableRefresh) return
+      this.touching = false
+      if (this.state === 2) { // in refreshing
         this.state = 2
         this.top = this.offset
-        this.onRefresh(this.refreshDone)
-      },
-      refreshDone() {
+        return
+      }
+      if (this.top >= this.offset) { // do refresh
+        this.refresh()
+      } else { // cancel refresh
         this.state = 0
         this.top = 0
-      },
-
-      infinite() {
-        this.infiniteLoading = true
-        this.onInfinite(this.infiniteDone)
-      },
-
-      infiniteDone() {
-        this.infiniteLoading = false
-      },
-      onScroll(e) {
-        if (!this.enableInfinite || this.infiniteLoading) {
-          return
-        }
-        let outerHeight = this.$el.clientHeight
-        let innerHeight = this.$el.querySelector('.inner').clientHeight
-        let scrollTop = this.$el.scrollTop
-        let ptrHeight = this.onRefresh ? this.$el.querySelector('.pull-refresh').clientHeight : 0
-        let infiniteHeight = this.$el.querySelector('.load-more').clientHeight
-        let bottom = innerHeight - outerHeight - scrollTop - ptrHeight
-        if (bottom < infiniteHeight) this.infinite()
-      },
-      onLoaded() {
-        this.$el.querySelector('.load-more').style.display = 'none';
       }
+    },
+    refresh() {
+      this.state = 2
+      this.top = this.offset
+      this.infiniteLoading = false
+      this.onPullUpText='加载中……'
+      this.onRefresh(this.refreshDone)
+    },
+    refreshDone() {
+      this.state = 0
+      this.top = 0
+      this.onPullUpText='加载完成'
+    },
+
+    infinite() {
+      this.infiniteLoading = true
+      this.onPullUpText='加载中……'
+      this.onInfinite(this.infiniteDone)
+    },
+
+    infiniteDone() {
+      this.infiniteLoading = false
+      this.onPullUpText='加载完成'
+    },
+
+    onLoaded() {
+      this.onPullUpText='没有更多';
+    },
+
+    onScroll(e) {
+      if (!this.enableInfinite || this.infiniteLoading) {
+        return false;
+      }
+      let outerHeight = this.$el.clientHeight
+      let innerHeight = this.$el.querySelector('.inner').clientHeight
+      let scrollTop = this.$el.scrollTop
+      let ptrHeight = this.onRefresh ? this.$el.querySelector('.pull-refresh').clientHeight : 0
+      let infiniteHeight = this.$el.querySelector('.load-more').clientHeight
+      let bottom = innerHeight - outerHeight - scrollTop - ptrHeight
+      if (bottom < infiniteHeight) this.infinite()
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -144,7 +152,6 @@
   left: 0;
   overflow: auto;
   -webkit-overflow-scrolling: touch;
-  background-color: #ddd
 }
 .anne-scroll .inner {
   position: absolute;
